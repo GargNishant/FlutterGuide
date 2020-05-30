ea999a636ad9a8c40b7ac391760f941149909d83
Nishant Garg <nishantgarg76342@gmail.com>
2020-05-30 16:17:16 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/eldersouls/flutterguide/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eldersouls.flutterguide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ApplicationWidget());
+
+class ApplicationWidget extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text("This is the Greatest application. Go!"));
+  }
+
+}
