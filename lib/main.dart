ea01eebb68c160c560893279c3185375635faf5b
Nishant Garg <nishantgarg76342@gmail.com>
2020-05-30 17:47:03 +0530

    + AppBar, Text, Scaffold, BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,22 @@ class ApplicationWidget extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("This is the Greatest application. Go!"));
+    return MaterialApp(
+        home: Scaffold( //Scaffold contains some basic elements of empty page like White background, themes for Text, etc
+          appBar: AppBar(title: Text("App Bar Text Widget"),), //Default App bar with a title
+
+          body: Text("This is my default Text"), //Body is the Background of the App.
+
+          bottomNavigationBar: BottomNavigationBar( //Bottom Navigation Bar
+            backgroundColor: Colors.amberAccent,
+            items: const <BottomNavigationBarItem>[  //Tabs of the Bottom Navigation bar are written here
+              BottomNavigationBarItem(icon: Icon(Icons.access_alarm),title: Text("Alarm")),
+              BottomNavigationBarItem(icon: Icon(Icons.access_time),title: Text("Time"))
+            ]
+         )
+
+        )
+    );
   }
 
 }
