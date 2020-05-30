eee23179adfdf6d693ac34aa218d0197cd76cdf3
Nishant Garg <nishantgarg76342@gmail.com>
2020-05-30 19:27:15 +0530

    *Button Function

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(ApplicationWidget());
 
-class ApplicationWidget extends StatelessWidget{
-
+class ApplicationWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var questions = ["What's you favorite color?","What's your favorite animal"];
+    var questions = [
+      "What's you favorite color?",
+      "What's your favorite animal"
+    ];
     return MaterialApp(
-        home: Scaffold( //Scaffold contains some basic elements of empty page like White background, themes for Text, etc
-          appBar: AppBar(title: Text("App Bar Text Widget"),), //Default App bar with a title
-          body: Column(children: <Widget>[
-            Padding(padding:EdgeInsets.only(top: 50,left: 10,),child: Text("Padded Text"),), // Padding Widget
-            Text("The Question!"),
-            RaisedButton(child: Text("Answer 1"),onPressed: null,),
-            RaisedButton(child: Text("Answer 2"),onPressed: null,),
-            RaisedButton(child: Text("Answer 3"),onPressed: null,),
-          ],
-          )
-
+        home: Scaffold(
+            //Scaffold contains some basic elements of empty page like White background, themes for Text, etc
+            appBar: AppBar(title: Text("App Bar Text Widget"),), //Default App bar with a title
+            body: Column(
+              children: <Widget>[
+                Text("The Question!"),
+                RaisedButton(child: Text("Answer 1"), onPressed: answerQuestion,), //We are passing the name of the method, making like a pointer. This way
+                RaisedButton(child: Text("Answer 2"), onPressed: answerQuestion,), //onPressed is not expecting the value of function, which is void.
+                RaisedButton(child: Text("Answer 3"), onPressed: answerQuestion,), //Instead it will execute the function when clicked. Lecture 31
+              ],
+            )
         )
     );
+  }
+
+  void answerQuestion() {
+    print("Answer Chosen!");
   }
 }
