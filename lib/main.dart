789a521946fb31c41c961ca3740640e333cddd01
Nishant Garg <nishantgarg76342@gmail.com>
2020-05-31 20:28:30 +0530

    Dynamic Answer Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:flutterguide/button.dart';
 
@@ -14,7 +16,28 @@ class ApplicationWidget extends StatefulWidget {
 
 class _ApplicationWidgetState extends State<ApplicationWidget>{
   var _questIndex = 0;
-  var _questions = ["What's you favorite color?", "What's your favorite animal?"];
+  var _questions = [
+    {
+      "quest":"What is you favourite Animal?",
+      "possibles": ["Dog","Cat","Birds","Ostrich"]
+    },
+    {
+      "quest":"What is you favourite Color?",
+      "possibles": ["Blue","Yellow","Black"]
+    },
+    {
+      "quest":"What is you favourite City?",
+      "possibles": ["X","Y","Z","Banglore","Hyderbad"]
+    },
+    {
+      "quest":"What is you favourite Job?",
+      "possibles": ["Android Development","Data Analyst","SDE","Mobile"]
+    },
+    {
+      "quest":"What is you favourite Game?",
+      "possibles": ["Souls","Witcher","GTA","Read Dead Redemption"]
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +46,10 @@ class _ApplicationWidgetState extends State<ApplicationWidget>{
             appBar: AppBar(title: Text("App Bar"),),
             body: Column(
               children: <Widget>[
-                StatelessText(_questions[_questIndex]),
-                Button(_answerQuestion,"Answer 1"),
-                Button(_answerQuestion,"Answer 2"),
-                Button(_answerQuestion,"Answer 3"),
+                StatelessText(_questions[_questIndex]['quest']),
+                ...(_questions[_questIndex]["possibles"] as List).map((answer) {
+                  return Button(_answerQuestion,answer);
+                }).toList(),
               ],
             )));
   }
@@ -34,8 +57,7 @@ class _ApplicationWidgetState extends State<ApplicationWidget>{
   void _answerQuestion() {
     setState((){
       _questIndex += 1;
-      _questIndex = _questIndex%2;
+      _questIndex = _questIndex%_questions.length;
     });
-    print(_questIndex);
   }
 }
