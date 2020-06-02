388745f6b377efefb973291402a23878c569bac8
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-02 20:37:39 +0530

    *Better Docs

--- lib/button.dart
+++ lib/button.dart
@@ -11,9 +11,9 @@ class Button extends StatelessWidget{
     return Container(
       width: double.infinity,
       margin: EdgeInsets.only(left: 10,right: 10,bottom: 5),
-      child: RaisedButton(onPressed: passedFunction,
-        splashColor: Colors.green,
-        color: Colors.greenAccent,
+      child: RaisedButton(onPressed: passedFunction, // When the Button is Pressed, Anonymous f'n will be executed. Inside the Anonymous Function is
+        splashColor: Colors.green,// answerQuestion(10). This answerQuestion refers to f'n in main.dart file. Thus, this Widget directly does not know
+        color: Colors.greenAccent,// score value, but because it was passed by parent inside, it can execute it
         child: Text(text),
       ),
     );
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -17,8 +17,10 @@ class QuizWidget extends StatelessWidget {
     return Column(
       children: <Widget>[
         StatelessText(questions[questIndex]['quest']),
-        ...(questions[questIndex]["possibles"] as List<Map<String, Object>>).map((answer) {
-          return Button(() { //Adding a new Anonymous F'n with void return type and no Parameter. Inside this, our parameterized fn can be called. This way we can pass the arguments
+        ...(questions[questIndex]["possibles"] as List<Map<String, Object>>)
+            .map((answer) {
+          return Button(() {
+            //Adding a new Anonymous F'n with void return type and no Parameter. Inside this, our parameterized fn is passed with a value of score. This way we can pass the arguments
             answerQuestion(answer['score']);
           }, answer["text"]);
         }).toList(),
